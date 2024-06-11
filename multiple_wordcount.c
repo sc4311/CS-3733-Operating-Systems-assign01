7ac82365373cf5c16d29d605239c653f461d559b
sc4311 <84731471+sc4311@users.noreply.github.com>
2024-06-11 09:08:50 -0500

    Comments

--- multiple_wordcount.c
+++ multiple_wordcount.c
@@ -4,6 +4,7 @@
 #include <sys/wait.h>
 
 int main(int argc, char *argv[]) {
+    // Check if the number of arguments is correct
     if (argc < 2) {
         printf("Usage: ./multiple_wordcount file1 file2 ... filen\n");
         return 1;
@@ -12,39 +13,55 @@ int main(int argc, char *argv[]) {
     int success_count = 0;
     int fail_count = 0;
 
+    // Loop over all the files provided as arguments
     for (int i = 1; i < argc; i++) {
+        // Create a new process
         pid_t pid = fork();
 
+        // If fork failed, print an error message and exit
         if (pid < 0) {
             perror("fork failed");
             exit(1);
         }
 
+        // If this is the child process
         if (pid == 0) {
+            // Prepare the arguments for the wordcount program
             char *args[] = {"./wordcount", argv[i], NULL};
+            // Execute the wordcount program
             if (execvp(args[0], args) == -1) {
+                // If exec failed, print an error message and exit
                 perror("exec failed");
                 exit(2);
             }
         }
     }
 
+    // Wait for all child processes to finish
     for (int i = 1; i < argc; i++) {
         int status;
         pid_t pid = wait(&status);
+        // If wait was successful
         if (pid != -1) {
+            // If the child process exited normally
             if (WIFEXITED(status)) {
+                // If the exit status was 0, increment the success count
                 if (WEXITSTATUS(status) == 0) {
                     success_count++;
-                } else if (WEXITSTATUS(status) == 1) {
+                }
+                // If the exit status was 1, increment the fail count
+                else if (WEXITSTATUS(status) == 1) {
                     fail_count++;
                 }
             }
         }
     }
 
+    // Print the number of child processes created and the number of files
     printf("Parent process %d created %d child processes to count words in %d files\n", getpid(), argc - 1, argc - 1);
+    // Print the number of files that have been counted successfully
     printf("%d files have been counted successfully!\n", success_count);
+    // Print the number of files that did not exist
     printf("%d files did not exist\n", fail_count);
 
     return 0;
--- wordcount.c
+++ wordcount.c
@@ -5,28 +5,35 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+// Function to read a line from a file
 char *readLine(FILE *file) {
     char *line = NULL;
     size_t len = 0;
     ssize_t read;
 
+    // Read a line from the file
     read = getline(&line, &len, file);
 
+    // If read is -1, free the line and return NULL
     if (read == -1) {
         free(line);
         return NULL;
     }
 
+    // Return the line read from the file
     return line;
 }
 
 int main(int argc, char *argv[]) {
+    // Check if the number of arguments is correct
     if (argc != 2) {
         printf("Usage: ./wordcount filename\n");
         return 1;
     }
 
+    // Open the file
     FILE *file = fopen(argv[1], "r");
+    // If the file cannot be opened, print an error message and return 1
     if (file == NULL) {
         printf("wordcount with process %d cannot open %s\n", getpid(), argv[1]);
         return 1;
@@ -35,17 +42,23 @@ int main(int argc, char *argv[]) {
     int count = 0;
     char *line;
 
+    // Read lines from the file until there are no more
     while ((line = readLine(file)) != NULL) {
+        // Tokenize the line into words
         char *word = strtok(line, " \t\n\r");
+        // Count the words
         while (word != NULL) {
             count++;
             word = strtok(NULL, " \t\n\r");
         }
+        // Free the line
         free(line);
     }
 
+    // Print the word count
     printf("wordcount with process %d counted words in %s: number of words is %d\n", getpid(), argv[1], count);
 
+    // Close the file
     fclose(file);
     return 0;
 }
